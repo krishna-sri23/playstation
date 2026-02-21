e2bdeb073ea6e2871fcd852518f7351ae381d880
Krishana <krishanasrivastava@nuvertos.com>
2026-02-21 23:07:07 +0530

    changes

---
+++ node-practice-repo/src/config/db.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';                                                                                                                                                                        
+                                                                                                                                                                                                                
+  const sequelize = new Sequelize(                                                                                                                                                                              
+    process.env.DB_NAME,                                                                                                                                                                                        
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      logging: true, /* to enable logging */
+      pool: {
+        max: 10,
+        min: 2,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
---
+++ node-practice-repo/src/middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+export class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+};
